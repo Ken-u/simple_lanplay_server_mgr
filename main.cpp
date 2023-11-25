61fdb0f39f93c26bdc4b252595fb64bca2540261
kenjc <kenjc.bian@rock-chips.com>
2023-11-25 10:51:37 +0800

    save data to local.
    Signed-off-by: kenjc <kenjc.bian@rock-chips.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <cstring>
+#include <vector>
+#include <algorithm>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -7,6 +11,40 @@
 
 const int PORT = 8080;
 const int BUFFER_SIZE = 1024;
+const char *SERVER_LIST_FILE = "server_list.txt";
+
+// Function to read server list from file
+std::vector<std::string> readServerList() {
+    std::vector<std::string> serverList;
+    std::ifstream file(SERVER_LIST_FILE);
+
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            serverList.push_back(line);
+        }
+        file.close();
+    }
+
+    return serverList;
+}
+
+// Function to write server list to file
+void writeServerList(const std::vector<std::string>& serverList) {
+    std::ofstream file(SERVER_LIST_FILE);
+
+    if (file.is_open()) {
+        for (const std::string& server : serverList) {
+            file << server << '\n';
+        }
+        file.close();
+    }
+}
+
+// Function to check if a server is already in the list
+bool isServerInList(const std::vector<std::string>& serverList, const std::string& newServer) {
+    return std::find(serverList.begin(), serverList.end(), newServer) != serverList.end();
+}
 
 void handle_client(int client_socket) {
     char buffer[BUFFER_SIZE];
@@ -46,9 +84,20 @@ void handle_client(int client_socket) {
                     // Check the parameter name
                     if (strcmp(param, "get_server_list") == 0) {
                         // Process the get_server_list parameter
-                        // Send the JSON response
-                        const char *response = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n[\"112.22\",\"233.33\"]";
-                        send(client_socket, response, strlen(response), 0);
+                        // Read server list from file
+                        std::vector<std::string> serverList = readServerList();
+
+                        // Send the JSON response with server list
+                        std::ostringstream response;
+                        response << "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n[";
+                        for (size_t i = 0; i < serverList.size(); ++i) {
+                            response << "\"" << serverList[i] << "\"";
+                            if (i < serverList.size() - 1) {
+                                response << ",";
+                            }
+                        }
+                        response << "]";
+                        send(client_socket, response.str().c_str(), response.str().length(), 0);
                     } else if (strcmp(param, "start_lanplay") == 0) {
                         // Process the start_lanplay parameter
                         // For simplicity, we'll just print the parameter value
@@ -67,14 +116,22 @@ void handle_client(int client_socket) {
                         // For simplicity, we'll just print the IP address
                         std::cout << "Add Server: " << paramValue << std::endl;
 
-                        // TODO: Add your code to handle the add_server parameter
-                        // You might want to parse the IP address and perform some action
-                        // For example, you could call a function like addServer(paramValue.c_str());
-                        // where addServer is a function you implement to handle add_server requests.
+                        // Check if the server is in the list
+                        std::vector<std::string> serverList = readServerList();
+                        if (!isServerInList(serverList, paramValue)) {
+                            // If not in the list, add to the list
+                            serverList.push_back(paramValue);
+                            // Update the file with the new server list
+                            writeServerList(serverList);
 
-                        // Send a response indicating that the add_server request was received
-                        const char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nAdd Server request received";
-                        send(client_socket, response, strlen(response), 0);
+                            // Send a response indicating that the add_server request was successful
+                            const char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nAdd Server request successful";
+                            send(client_socket, response, strlen(response), 0);
+                        } else {
+                            // If the server is already in the list, send a response indicating that it already exists
+                            const char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nServer already exists in the list";
+                            send(client_socket, response, strlen(response), 0);
+                        }
                     }
                 }
 
